b657df095c850ef1da185a002274d73563a56833
jdvolk <justin.d.volk@gmail.com>
2021-01-09 18:59:49 -0700

    add hook to dynamically render form based on state and an array of element titles

---
+++ src/app/Routes/useDynamicForm.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-else-return */
+/* eslint-disable consistent-return */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable arrow-body-style */
+import React from 'react';
+
+const useDynamicForm = (state, elements, setState) => {
+  const typeHandler = (type) => {
+    if (type === 'email' || type === 'password') {
+      return type;
+    } else {
+      return 'text';
+    }
+  };
+
+  return elements.map((element) => {
+    const lowercase = element.toLowerCase();
+    return (
+      <label>
+        { `${element}: ` }
+        <input
+          type={typeHandler(lowercase)}
+          name={`${lowercase}-input`}
+          className={`${lowercase}-input text-input`}
+          onChange={(e) => {
+            setState((prevState) => {
+              return { ...prevState, [element]: e.target.value };
+            });
+          }}
+          value={state.element}
+          placeholder={`${element}`}
+        />
+      </label>
+    );
+  });
+};
+
+export default useDynamicForm;
