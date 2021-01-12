1bde007063d88e17d79e132fc80733df7ee927cf
jdvolk <justin.d.volk@gmail.com>
2021-01-11 17:37:36 -0700

    import hook to create dynamic forms, change component state to match what hook needs

--- src/app/Routes/Login/Login.js
+++ src/app/Routes/Login/Login.js
@@ -1,32 +1,59 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { useSelector, useDispatch } from 'react-redux';
+
+// reduxState
 import { Redirect } from 'react-router-dom';
 import { getPosts } from '../../../features/User/userPostsSlice';
-import {
-  getUser,
-  selectIsLoggedIn,
-} from '../../../features/User/getUserSlice';
-import renderLoginForm from './renderLoginForm';
+import { getUser, selectIsLoggedIn } from '../../../features/User/getUserSlice';
+
+// UI
+import Button from '../../Components/Button/Button';
+import '../../Components/Button/Button.css';
+
+// custom hooks
+import useDynamicForm from '../useDynamicForm';
 
 function Login() {
   const dispatch = useDispatch();
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const [formState, setFormState] = useState({
+    Email: '',
+    Password: '',
+  });
+  const password = formState.Password;
+  const email = formState.Email;
+  const formElements = ['Email', 'Password'];
+
   return (
-    renderLoginForm(
-      getPosts,
-      getUser,
-      userName,
-      password,
-      setUserName,
-      setPassword,
-      isLoggedIn,
-      Redirect,
-      dispatch,
-    )
+    <form
+      className="login-form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        getPosts();
+        getUser({ email, password });
+      }}
+    >
+      <h1>Login</h1>
+      {formElements ? useDynamicForm(formState, formElements, setFormState) : null}
+      <section>{'\n'}</section>
+      <Button
+        type="submit"
+        className="login-button"
+        value="login"
+        name="login-button"
+        label="Login"
+        onClick={() => {
+          dispatch(getPosts());
+          dispatch(getUser({ email, password }));
+        }}
+      >
+        Login
+      </Button>
+      {isLoggedIn && <Redirect to="/" />}
+    </form>
   );
 }
 
