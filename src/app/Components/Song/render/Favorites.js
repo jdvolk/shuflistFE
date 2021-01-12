44efdd00ec5da6eb41eeb0f9b520c5c2b92ebb52
jdvolk <justin.d.volk@gmail.com>
2021-01-11 17:17:46 -0700

    remove whitespace

--- src/app/Components/Song/render/Favorites.js
+++ src/app/Components/Song/render/Favorites.js
@@ -1,7 +1,6 @@
 /* eslint-disable consistent-return */
 /* eslint-disable arrow-body-style */
 import React from 'react';
-
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 const renderFavorites = (passedSong, handleFavClick) => {
