9760f92b5f9b86681106b7d97cd7fedbeb6e6c22
jdvolk <justin.d.volk@gmail.com>
2021-01-11 17:19:01 -0700

    add index for id

--- src/app/Routes/SignUpForm/SignUpForm.js
+++ src/app/Routes/SignUpForm/SignUpForm.js
@@ -1,6 +1,8 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
+import Dropzone from 'react-dropzone-uploader';
+import 'react-dropzone-uploader/dist/styles.css';
 import Button from '../../Components/Button/Button';
 import '../../Components/Button/Button.css';
 import useDynamicForm from '../useDynamicForm';
@@ -8,6 +10,10 @@ import '../Login/Login.css';
 // import renderLoginForm from '../Login/renderLoginForm';
 import './SignUpForm.css';
 
+// https://react-dropzone-uploader.js.org/docs/quick-start
+
+// TODO: handle sumbit for entire form, handle image uploader connect the form to a post request
+// figure out props for dropzone
 function SignUpForm() {
   const [formState, setFormState] = useState({
     Email: '',
@@ -30,7 +36,11 @@ function SignUpForm() {
       >
         <h1>Sign Up</h1>
         {formElements ? useDynamicForm(formState, formElements, setFormState) : null}
-        <Button type="submit" className="sign-up-submit" label="Sign Up" />
+        <label>
+          User Image:
+        </label>
+        <Dropzone key="image-upload" />
+        <Button type="submit" className="sign-up-submit" label="Sign Up" key="button" />
       </form>
     </section>
   );
