9fca69635e9e5c27c901bd36803c65b2dc51f51d
jdvolk <justin.d.volk@gmail.com>
2021-01-21 03:23:25 -0700

    add route obj for default, loggedIn, and loggedOut, add map function that takes theses arrays and renders link based on conditional rendering

--- src/app/Components/Navagation/Navigation.js
+++ src/app/Components/Navagation/Navigation.js
@@ -1,3 +1,4 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/anchor-is-valid */
@@ -13,51 +14,58 @@ import { resetSearch } from '../../../features/SongSearch/songInputSlice';
 import './Navigation.css';
 
 function Navigation(props) {
+  const { history } = props;
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
 
+  const defaultNav = [
+    { label: 'Home', route: '/', Click: () => dispatch(resetSearch()) },
+    { label: 'Back', route: '#/', Click: () => history.goBack() },
+    { label: 'Search', route: '/Search', Click: () => null },
+  ];
+
+  const loggedIn = [
+    ...defaultNav,
+    { label: 'Favorites', route: '/Favorites', Click: () => dispatch(resetSearch()) },
+    { label: 'Logout', route: '/', Click: () => dispatch(logout()) && dispatch(resetSearch()) },
+  ];
+
+  const loggedOut = [
+    { label: 'Login', route: '/Login', Click: () => null },
+    ...defaultNav,
+  ];
+
+  const renderNav = (location) => location.map((link) => {
+    return (
+      <Link
+        key={link.label}
+        to={`${link.route}`}
+        onClick={link.Click}
+      >
+        {link.label}
+      </Link>
+    );
+  });
+
   return (
     <section className="nav-bar">
-      <Link
-        to="/"
-        onClick={() => {
-          dispatch(resetSearch());
-        }}
-      >
-        Home
-      </Link>
-      <Link
-        to="#/"
-        onClick={() => props.history.goBack()}
-      >
-        Back
-      </Link>
-      {!isLoggedIn
-        ? <Link to="/Login"> Login </Link>
-        : (
+      {isLoggedIn && renderNav(loggedIn)}
+      {!isLoggedIn && renderNav(loggedOut)}
+      {/* <Link to="/" onClick={() => dispatch(resetSearch())}>Home</Link>
+      <Link to="#/" onClick={() => history.goBack()}>Back</Link>
+      {!isLoggedIn && <Link to="/Login"> Login </Link>}
+      { isLoggedIn && (
+        <>
           <Link
             to="/"
-            onClick={() => {
-              dispatch(logout()) && dispatch(resetSearch());
-            }}
+            onClick={() => dispatch(logout()) && dispatch(resetSearch())}
           >
             Logout
           </Link>
-        )}
-      <Link
-        to="/Favorites"
-        onClick={() => {
-          dispatch(resetSearch());
-        }}
-      >
-        Favorites
-      </Link>
-      <Link
-        to="/Search"
-      >
-        Search
-      </Link>
-      {/* <Link to="/options">Options</Link> */}
+          <Link to="/Favorites" onClick={() => dispatch(resetSearch())}>Favorites</Link>
+        </>
+      )}
+      <Link to="/Search">Search</Link> */}
     </section>
   );
 }
