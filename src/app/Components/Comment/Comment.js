65e1788db3c3d6b5badf963faa78c066bcd16df7
jdvolk <justin.d.volk@gmail.com>
2021-11-05 13:22:07 -0600

    adds comment header with profile picture to comments and correct styling

--- src/app/Components/Comment/Comment.js
+++ src/app/Components/Comment/Comment.js
@@ -1,15 +1,20 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import UserHeader from '../UserHeader/UserHeader';
 
 function Comment(props) {
   // eslint-disable-next-line react/prop-types
   if (props) {
     return (
-      <section>
-        <p>{props.Author}</p>
-        <p>{props.Body}</p>
+      <section className="comment">
+        <UserHeader />
+        <section className="comment-body">
+          <p>{props.Author} </p>
+          <p> - {props.Body}</p>
+        </section>
       </section>
     );
   }
